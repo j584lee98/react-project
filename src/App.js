51baab0212ff4543408f2d04b516283c242f53f6
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-01-03 15:56:36 -0500

    fixed homepage background video responsiveness

--- src/App.js
+++ src/App.js
@@ -18,8 +18,27 @@ function App() {
   );
 }
 
-function Home() {
-  return (
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      height: 0,
+      width: 0
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        height: window.innerHeight,
+        width: document.body.offsetWidth
+
+      });
+  }, 50);
+  }
+
+  render() {
+    return (
     <div className="home">
       <Nav />
       <div className="section">
@@ -28,7 +47,13 @@ function Home() {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla dui enim, non pellentesque tellus dignissim eget. Integer ut fermentum ex. Sed ut enim justo. Nulla vitae pellentesque ex. Phasellus elit nulla, imperdiet ut ultrices eget, molestie eu orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
         <div className="video-container">
-          <video autoPlay loop muted width='100%'>
+          <video
+            autoPlay
+            loop
+            muted
+            height={3840/this.state.width > 2160/this.state.height ? '100vh' : null}
+            width={3840/this.state.width <= 2160/this.state.height ? '100vw' : null}
+          >
             <source src={Video} type="video/mp4"></source>
           </video>
         </div>
@@ -38,6 +63,7 @@ function Home() {
       <div className="box"></div>
     </div>
   );
+  }
 }
 
 export default App;
