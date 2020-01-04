ba80d38e48f558c5aaed5337ddc65dc6877ace78
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-01-04 10:37:05 -0500

    added two transitions for homepage background

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import About from './About';
 import Nav from './Nav';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Video from './static/night.mp4'
+import { Animated } from "react-animated-css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/triangle" component={Triangle} />
           <Route path="/about" component={About} />
         </Switch>
       </div>
@@ -31,38 +35,105 @@ class Home extends React.Component {
     setInterval(() => {
       this.setState({
         height: window.innerHeight,
-        width: document.body.offsetWidth
-
+        width: document.body.offsetWidth,
+        offset: window.pageYOffset
       });
-  }, 50);
+    }, 50);
   }
 
   render() {
     return (
-    <div className="home">
-      <Nav />
-      <div className="section">
-        <div className="section-text">
-          <h1>Home Page</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla dui enim, non pellentesque tellus dignissim eget. Integer ut fermentum ex. Sed ut enim justo. Nulla vitae pellentesque ex. Phasellus elit nulla, imperdiet ut ultrices eget, molestie eu orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <div className="home">
+        <Nav />
+        <div className="section">
+          <div className="section-text">
+            <h1>Home Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla dui enim, non pellentesque tellus dignissim eget. Integer ut fermentum ex. Sed ut enim justo. Nulla vitae pellentesque ex. Phasellus elit nulla, imperdiet ut ultrices eget, molestie eu orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <Animated animationIn="heartBeat" infinite isVisible={true}>
+              <div>
+                <FontAwesomeIcon className="icon" icon={faAngleDoubleDown} size="4x" onClick={() => document.querySelector('div.box').scrollIntoView({ behavior: 'smooth' })} />
+              </div>
+            </Animated>
+          </div>
+          <div className="video-container" style={{ opacity: 1 - this.state.offset / this.state.height }}>
+            <video
+              autoPlay
+              loop
+              muted
+              height={3840 / this.state.width > 2160 / this.state.height ? '100vh' : null}
+              width={3840 / this.state.width <= 2160 / this.state.height ? '100vw' : null}
+            >
+              <source src={Video} type="video/mp4"></source>
+            </video>
+          </div>
         </div>
-        <div className="video-container">
-          <video
-            autoPlay
-            loop
-            muted
-            height={3840/this.state.width > 2160/this.state.height ? '100vh' : null}
-            width={3840/this.state.width <= 2160/this.state.height ? '100vw' : null}
-          >
-            <source src={Video} type="video/mp4"></source>
-          </video>
-        </div>
+        <div className="box" style={{
+          position: 'absolute',
+          top: (this.state.height + 100) + 'px',
+          left: 0,
+        }}><h1>Container</h1></div>
       </div>
-      <div className="box"></div>
-      <div className="box"></div>
-      <div className="box"></div>
-    </div>
-  );
+    );
+  }
+}
+
+class Triangle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      height: 0,
+      width: 0
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        height: window.innerHeight,
+        width: document.body.offsetWidth,
+        offset: window.pageYOffset
+      });
+    }, 50);
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <Nav />
+        <div className="section">
+          <div className="section-text">
+            <h1>Home Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla dui enim, non pellentesque tellus dignissim eget. Integer ut fermentum ex. Sed ut enim justo. Nulla vitae pellentesque ex. Phasellus elit nulla, imperdiet ut ultrices eget, molestie eu orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <Animated animationIn="heartBeat" infinite isVisible={true}>
+              <div>
+                <FontAwesomeIcon className="icon" icon={faAngleDoubleDown} size="4x" onClick={() => document.querySelector('div.box').scrollIntoView({ behavior: 'smooth' })} />
+              </div>
+            </Animated>
+          </div>
+          <div className="video-container">
+            <video
+              autoPlay
+              loop
+              muted
+              height={3840 / this.state.width > 2160 / this.state.height ? '100vh' : null}
+              width={3840 / this.state.width <= 2160 / this.state.height ? '100vw' : null}
+            >
+              <source src={Video} type="video/mp4"></source>
+            </video>
+          </div>
+        </div>
+        <div className="triangle" style={{
+          borderLeft: this.state.width / 2 + 'px solid transparent',
+          borderRight: this.state.width / 2 + 'px solid transparent',
+          borderBottom: '100px solid white',
+        }}></div>
+        <div className="box" style={{
+          position: 'absolute',
+          top: (this.state.height + 100) + 'px',
+          left: 0,
+        }}><h1>Container</h1></div>
+      </div>
+    );
   }
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -2,9 +2,29 @@ import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 
-function Nav() {
-  return (
-    <div id="navbar">
+class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      height: 0,
+      width: 0
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        height: window.innerHeight,
+        width: document.body.offsetWidth,
+        offset: window.pageYOffset
+      });
+    }, 50);
+  }
+
+  render() {
+    return (
+    // <div id="navbar" style={{backgroundColor: this.state.offset < this.state.height ? 'rgba(33, 37, 41, 0.2)' : 'rgb(33, 37, 41)'}}>
+    <div id="navbar" style={{backgroundColor: 'rgba(33, 37, 41, ' + (0.2 + (1-0.2)*(this.state.offset/this.state.height)) + ')'}}>
       <nav className="navbar navbar-expand-lg navbar-dark">
         <a className="navbar-brand" href="/">Project</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,10 +36,7 @@ function Nav() {
               <a className="nav-link" href="/">Home<span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item active">
-              <a className="nav-link" href="/">Blank</a>
-            </li>
-            <li className="nav-item active">
-              <a className="nav-link" href="/">Blank</a>
+              <a className="nav-link" href="/triangle">Triangle</a>
             </li>
             <li className="nav-item active">
               <a className="nav-link" href="/about">About</a>
@@ -33,6 +50,8 @@ function Nav() {
       </nav>
     </div>
   );
+  }
+  
 }
 
 export default Nav;
