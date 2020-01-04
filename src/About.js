11eb429ca29ad368d86f2ab372ac01c7fb1480f4
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-01-04 18:04:38 -0500

    added two different formats for member cards in About

--- src/About.js
+++ src/About.js
@@ -1,70 +1,205 @@
 import React from 'react';
 import './App.css';
 import Nav from './Nav'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function About() {
   return (
-    <div className="about">
+    <div className="about-main">
       <Nav />
-      <div class="container">
-        <div class="row text-center">
+      <div className="about-content">
+        <div className="row text-center">
 
-          <div class="container py-5">
-            <div class="row text-center text-white">
-              <div class="col-lg-8 mx-auto">
-                <h1 class="display-4">Our Team</h1>
+          <div className="container py-5">
+            <div className="row text-center text-white">
+              <div className="col-lg-8 mx-auto">
+                <h1 className="display-4">Our Team</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla dui enim, non pellentesque tellus dignissim eget. Integer ut fermentum ex. Sed ut enim justo. Nulla vitae pellentesque ex. Phasellus elit nulla, imperdiet ut ultrices eget, molestie eu orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
               </div>
             </div>
           </div>
 
-          <div class="col-xl-3 col-sm-6 mb-5">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
-              <h5 class="mb-0">Member Name</h5><span class="small text-uppercase text-muted">Profession</span>
-              <ul class="social mb-0 list-inline mt-3">
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          <div className="horizontal col-xl-3 col-sm-6 mb-5">
+            <div className="bg-white rounded shadow-sm">
+              <div className="picture rounded-top"></div>
+              <div className="p-4">
+                <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
+                </ul>
+                <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
+                <hr></hr>
+                <p>Vestibulum luctus dolor elit, faucibus tincidunt erat tempor quis. In condimentum eget eros id bibendum.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="horizontal col-xl-3 col-sm-6 mb-5">
+            <div className="bg-white rounded shadow-sm">
+              <div className="picture rounded-top"></div>
+              <div className="p-4">
+                <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
+                </ul>
+                <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
+                <hr></hr>
+                <p>Vestibulum luctus dolor elit, faucibus tincidunt erat tempor quis. In condimentum eget eros id bibendum.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="horizontal col-xl-3 col-sm-6 mb-5">
+            <div className="bg-white rounded shadow-sm">
+              <div className="picture rounded-top"></div>
+              <div className="p-4">
+                <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
+                </ul>
+                <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
+                <hr></hr>
+                <p>Vestibulum luctus dolor elit, faucibus tincidunt erat tempor quis. In condimentum eget eros id bibendum.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="horizontal col-xl-3 col-sm-6 mb-5">
+            <div className="bg-white rounded shadow-sm">
+              <div className="picture rounded-top"></div>
+              <div className="p-4">
+                <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
+                </ul>
+                <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
+                <hr></hr>
+                <p>Vestibulum luctus dolor elit, faucibus tincidunt erat tempor quis. In condimentum eget eros id bibendum.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="vertical col-xl-6 col-lg-6 mb-5">
+            <div className="testing bg-white rounded shadow-sm">
+              <div className="picture rounded-left"></div>
+              <div className="desc p-4">
+                <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
+                </ul>
+                <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
+                <hr></hr>
+                <p>Vestibulum luctus dolor elit, faucibus tincidunt erat tempor quis. In condimentum eget eros id bibendum.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="vertical col-xl-6 col-lg-6 mb-5">
+            <div className="testing bg-white rounded shadow-sm">
+              <div className="picture rounded-left"></div>
+              <div className="desc p-4">
+                <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
+                </ul>
+                <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
+                <hr></hr>
+                <p>Vestibulum luctus dolor elit, faucibus tincidunt erat tempor quis. In condimentum eget eros id bibendum.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="vertical col-xl-6 col-lg-6 mb-5">
+            <div className="testing bg-white rounded shadow-sm">
+              <div className="picture rounded-left"></div>
+              <div className="desc p-4">
+                <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
+                </ul>
+                <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
+                <hr></hr>
+                <p>Vestibulum luctus dolor elit, faucibus tincidunt erat tempor quis. In condimentum eget eros id bibendum.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="vertical col-xl-6 col-lg-6 mb-5">
+            <div className="testing bg-white rounded shadow-sm">
+              <div className="picture rounded-left"></div>
+              <div className="desc p-4">
+                <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+                <ul className="social mb-0 list-inline mt-3">
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                  <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
+                </ul>
+                <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
+                <hr></hr>
+                <p>Vestibulum luctus dolor elit, faucibus tincidunt erat tempor quis. In condimentum eget eros id bibendum.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-xl-3 col-sm-6 mb-5">
+            <div className="bg-white rounded shadow-sm py-5 px-4"><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
+              <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+              <ul className="social mb-0 list-inline mt-3">
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
               </ul>
               <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
             </div>
           </div>
 
-          <div class="col-xl-3 col-sm-6 mb-5">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
-              <h5 class="mb-0">Member Name</h5><span class="small text-uppercase text-muted">Profession</span>
-              <ul class="social mb-0 list-inline mt-3">
-                <li class="list-inline-item"><a href="#" class="social-link"><i className="fab fa-facebook-f"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fas fa-twitter"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fas fa-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fas fa-linkedin"></i></a></li>
+          <div className="col-xl-3 col-sm-6 mb-5">
+            <div className="bg-white rounded shadow-sm py-5 px-4"><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
+              <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+              <ul className="social mb-0 list-inline mt-3">
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
               </ul>
               <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
             </div>
           </div>
 
-          <div class="col-xl-3 col-sm-6 mb-5">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
-              <h5 class="mb-0">Member Name</h5><span class="small text-uppercase text-muted">Profession</span>
-              <ul class="social mb-0 list-inline mt-3">
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          <div className="col-xl-3 col-sm-6 mb-5">
+            <div className="bg-white rounded shadow-sm py-5 px-4"><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
+              <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+              <ul className="social mb-0 list-inline mt-3">
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
               </ul>
               <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
             </div>
           </div>
 
-          <div class="col-xl-3 col-sm-6 mb-5">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
-              <h5 class="mb-0">Member Name</h5><span class="small text-uppercase text-muted">Profession</span>
-              <ul class="social mb-0 list-inline mt-3">
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          <div className="col-xl-3 col-sm-6 mb-5">
+            <div className="bg-white rounded shadow-sm py-5 px-4"><img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
+              <h5 className="mb-0">Member Name</h5><span className="small text-uppercase text-muted">Profession</span>
+              <ul className="social mb-0 list-inline mt-3">
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a></li>
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a></li>
+                <li className="list-inline-item"><a href="#" className="linkicon"><FontAwesomeIcon icon={faGithub} size="2x" /></a></li>
               </ul>
               <p>Suspendisse efficitur, eros ac congue faucibus, diam arcu eleifend quam, ac sagittis elit lorem eget leo. Nunc mollis ornare condimentum.</p>
             </div>
--- src/App.js
+++ src/App.js
@@ -43,10 +43,10 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div className="home">
+      <div className="home-main">
         <Nav />
-        <div className="section">
-          <div className="section-text">
+        <div className="home-content">
+          <div className="background">
             <h1>Home Page</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla dui enim, non pellentesque tellus dignissim eget. Integer ut fermentum ex. Sed ut enim justo. Nulla vitae pellentesque ex. Phasellus elit nulla, imperdiet ut ultrices eget, molestie eu orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <Animated animationIn="heartBeat" infinite isVisible={true}>
@@ -98,10 +98,10 @@ class Triangle extends React.Component {
 
   render() {
     return (
-      <div className="home">
+      <div className="home-main">
         <Nav />
-        <div className="section">
-          <div className="section-text">
+        <div className="home-content">
+          <div className="background">
             <h1>Home Page</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fringilla dui enim, non pellentesque tellus dignissim eget. Integer ut fermentum ex. Sed ut enim justo. Nulla vitae pellentesque ex. Phasellus elit nulla, imperdiet ut ultrices eget, molestie eu orci. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             <Animated animationIn="heartBeat" infinite isVisible={true}>
