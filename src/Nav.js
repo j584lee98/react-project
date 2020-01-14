8ba0eaca6093672a307115b8670e025f8d73aa31
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-01-13 21:55:05 -0500

    removed search bar and edited styles for scrollbar and member cards

--- src/Nav.js
+++ src/Nav.js
@@ -26,7 +26,7 @@ class Nav extends React.Component {
     // <div id="navbar" style={{backgroundColor: this.state.offset < this.state.height ? 'rgba(33, 37, 41, 0.2)' : 'rgb(33, 37, 41)'}}>
     <div id="navbar" style={{backgroundColor: 'rgba(33, 37, 41, ' + (0.2 + (1-0.2)*(this.state.offset/this.state.height)) + ')'}}>
       <nav className="navbar navbar-expand-lg navbar-dark">
-        <a className="navbar-brand" href="/">{this.state.width}</a>
+        <a className="navbar-brand" href="/">L3 Capital</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -42,10 +42,6 @@ class Nav extends React.Component {
               <a className="nav-link" href="/about">About</a>
             </li>
           </ul>
-          <form className="form-inline">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-            <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-          </form>
         </div>
       </nav>
     </div>
